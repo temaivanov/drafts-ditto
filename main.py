03f305446da74a260afe1a0c7458257becb7c017
artem <artyom-ivanov-spb@yandex.ru>
2023-03-05 00:10:05 +0300

    vs code test commit 2

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #run to start the app on the server
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,2 +1,9 @@
 #__init__.py to make the website folder a python package
+from flask import Flask 
 
+#setting up Flask goes here
+def create_app():
+    app = Flask(__name__) #https://blog.miguelgrinberg.com/post/why-do-we-pass-name-to-the-flask-class
+    app.config.from_pyfile('../config.py') #https://hackersandslackers.com/configure-flask-applications/ should I put .env into a .gitignore?
+
+    return app
