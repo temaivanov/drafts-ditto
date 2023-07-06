d7dfeb2162bd33570e110157f82ce2454dd3f738
artem <artyom-ivanov-spb@yandex.ru>
2023-07-06 23:08:11 +0300

    no message

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 #here we will store the map for our website, meaning the places the user can navigate to. 
 #Everything that is not related to authentication, becase we have auth.py for that
+#test comment July7th 
 
 from flask import Blueprint
